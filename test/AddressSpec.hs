3e0ba4f6b78bcccc97e7eb3bae79cce7f9016c11
Sibi Prabakaran <sibi@psibi.in>
2019-01-20 02:38:54 +0530

    Add more test

--- test/AddressSpec.hs
+++ test/AddressSpec.hs
@@ -1,13 +1,25 @@
+{-#LANGUAGE ScopedTypeVariables#-}
+
 module AddressSpec where
 
 import Faker
 import Faker.Address
 import Test.Hspec
 import qualified Data.Vector as V
+import Config
+import Data.Text (Text)
 
 spec :: Spec
 spec = do
   describe "Address" $ do
-         it "parses countries" $ do
-                         ctries <- countries defaultFakerSettings
-                         ctries `shouldSatisfy` (\x -> V.length x >= 40)
+    it "parses countries for defaultSettings" $ do
+      ctries <- countries defaultFakerSettings
+      ctries `shouldSatisfy` (\x -> V.length x >= 40)
+    it "parses countries for other locales" $ do
+      locales <- populateLocales
+      let settings :: [FakerSettings] =
+            map (\l -> setLocale l defaultFakerSettings) locales
+          ctries :: IO [V.Vector Text] = mapM countries settings
+      ctries' :: [V.Vector Text] <- ctries
+      let exp :: [Bool] = map (\x -> V.length x >= 10) ctries'
+      True `shouldBe` (all (\x -> x == True) exp)
