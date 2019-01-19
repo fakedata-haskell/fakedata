ca2729800ae56f9c8a7864c635ff343df85601a2
Sibi Prabakaran <sibi@psibi.in>
2019-01-20 02:38:18 +0530

    Add functions for getting other locales

--- src/Config.hs
+++ src/Config.hs
@@ -1,5 +1,10 @@
 module Config where
 
+import System.Directory (listDirectory, doesFileExist)
+import Control.Monad (filterM)
+import Data.Text (Text, pack)
+import System.FilePath (takeFileName, takeExtension)
+
 defaultEnDirectory :: FilePath
 defaultEnDirectory = "faker/lib/locales/en"
 
@@ -8,3 +13,19 @@ localesDirectory = "faker/lib/locales"
 
 localesEnDirectory :: FilePath
 localesEnDirectory = "faker/lib/locales/en"
+
+isLocaleFile :: FilePath -> IO Bool
+isLocaleFile fname = do
+  exist <- doesFileExist fname
+  pure (exist && (takeExtension fname == ".yml"))
+
+listLocaleFiles :: FilePath -> IO [FilePath]
+listLocaleFiles fname = do
+  files <- listDirectory fname
+  filterM isLocaleFile files
+
+populateLocales :: IO [Text]
+populateLocales = do
+  files <- listLocaleFiles localesDirectory
+  let files' = map (pack . takeFileName) files
+  pure files'
