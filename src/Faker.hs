3fe415a09303aef12d2d18ed8865d8d69339828a
Sibi Prabakaran <sibi@psibi.in>
2019-01-20 12:44:42 +0530

    Add more settings

--- src/Faker.hs
+++ src/Faker.hs
@@ -5,6 +5,7 @@ module Faker
     ( FakerSettings,
       defaultFakerSettings,
       setLocale,
+      setRandomGen,
       FakerException (..),
       fakerLocale
     ) where
@@ -12,17 +13,21 @@ module Faker
 import Control.Exception (Exception)
 import Data.Text
 import Data.Typeable
+import System.Random
 
-data FakerSettings = FakerSettings { locale :: Text } deriving (Show, Eq, Ord)
+data FakerSettings = FakerSettings { locale :: Text, randomGen :: StdGen } deriving (Show)
 
 data FakerException = InvalidLocale String deriving (Typeable, Show)
 instance Exception FakerException
 
 defaultFakerSettings :: FakerSettings
-defaultFakerSettings = FakerSettings { locale = "en" }
+defaultFakerSettings = FakerSettings { locale = "en", randomGen = (mkStdGen 10000) }
 
 setLocale :: Text -> FakerSettings -> FakerSettings
 setLocale localeTxt fs = fs { locale = localeTxt }
+
+setRandomGen :: StdGen -> FakerSettings -> FakerSettings
+setRandomGen gen fs = fs { randomGen = gen}
 
 fakerLocale :: FakerSettings -> Text
 fakerLocale FakerSettings {..} = locale
