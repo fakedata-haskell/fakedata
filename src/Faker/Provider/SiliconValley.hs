9bfcbf1a1f76f0be6f9570994922968e8d2a5e2d
Sibi Prabakaran <sibi@psibi.in>
2019-02-23 19:33:24 +0530

    SiliconValley

---
+++ src/Faker/Provider/SiliconValley.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Provider.SiliconValley where
+
+import Config
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Data.Yaml
+import Faker
+import Faker.Internal
+import Faker.Provider.TH
+import Language.Haskell.TH
+
+parseSiliconValley :: FromJSON a => FakerSettings -> Value -> Parser a
+parseSiliconValley settings (Object obj) = do
+  en <- obj .: (getLocale settings)
+  faker <- en .: "faker"
+  siliconValley <- faker .: "silicon_valley"
+  pure siliconValley
+parseSiliconValley settings val = fail $ "expected Object, but got " <> (show val)
+
+parseSiliconValleyField ::
+     (FromJSON a, Monoid a) => FakerSettings -> Text -> Value -> Parser a
+parseSiliconValleyField settings txt val = do
+  siliconValley <- parseSiliconValley settings val
+  field <- siliconValley .:? txt .!= mempty
+  pure field
+
+parseSiliconValleyFields ::
+     (FromJSON a, Monoid a) => FakerSettings -> [Text] -> Value -> Parser a
+parseSiliconValleyFields settings txts val = do
+  siliconValley <- parseSiliconValley settings val
+  helper siliconValley txts
+  where
+    helper :: (FromJSON a) => Value -> [Text] -> Parser a
+    helper a [] = parseJSON a
+    helper (Object a) (x:xs) = do
+      field <- a .: x
+      helper field xs
+    helper a (x:xs) = fail $ "expect Object, but got " <> (show a)
+
+
+
+
+$(genParser "siliconValley" "characters")
+
+$(genProvider "siliconValley" "characters")
+
+
+$(genParser "siliconValley" "companies")
+
+$(genProvider "siliconValley" "companies")
+
+
+$(genParser "siliconValley" "quotes")
+
+$(genProvider "siliconValley" "quotes")
+
+
+$(genParser "siliconValley" "apps")
+
+$(genProvider "siliconValley" "apps")
+
+
+$(genParser "siliconValley" "inventions")
+
+$(genProvider "siliconValley" "inventions")
+
+
+$(genParser "siliconValley" "mottos")
+
+$(genProvider "siliconValley" "mottos")
+
+
+$(genParser "siliconValley" "urls")
+
+$(genProvider "siliconValley" "urls")
+
+
+$(genParser "siliconValley" "email")
+
+$(genProvider "siliconValley" "email")
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Faker/SiliconValley.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.SiliconValley where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.SiliconValley
+import Faker.TH
+
+
+$(generateFakeField "siliconValley" "characters")
+
+$(generateFakeField "siliconValley" "companies")
+
+$(generateFakeField "siliconValley" "quotes")
+
+$(generateFakeField "siliconValley" "apps")
+
+$(generateFakeField "siliconValley" "inventions")
+
+$(generateFakeField "siliconValley" "mottos")
+
+$(generateFakeField "siliconValley" "urls")
+
+$(generateFakeField "siliconValley" "email")
+
+
+
+
+
+
