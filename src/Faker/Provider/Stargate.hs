98e22be55a36d5a8323507bb87129ce3a8f5c303
Sibi Prabakaran <sibi@psibi.in>
2019-02-23 23:01:43 +0530

    StarGate

---
+++ src/Faker/Provider/Stargate.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Provider.Stargate where
+
+import Config
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Data.Yaml
+import Faker
+import Faker.Internal
+import Faker.Provider.TH
+import Language.Haskell.TH
+
+parseStargate :: FromJSON a => FakerSettings -> Value -> Parser a
+parseStargate settings (Object obj) = do
+  en <- obj .: (getLocale settings)
+  faker <- en .: "faker"
+  stargate <- faker .: "stargate"
+  pure stargate
+parseStargate settings val = fail $ "expected Object, but got " <> (show val)
+
+parseStargateField ::
+     (FromJSON a, Monoid a) => FakerSettings -> Text -> Value -> Parser a
+parseStargateField settings txt val = do
+  stargate <- parseStargate settings val
+  field <- stargate .:? txt .!= mempty
+  pure field
+
+parseStargateFields ::
+     (FromJSON a, Monoid a) => FakerSettings -> [Text] -> Value -> Parser a
+parseStargateFields settings txts val = do
+  stargate <- parseStargate settings val
+  helper stargate txts
+  where
+    helper :: (FromJSON a) => Value -> [Text] -> Parser a
+    helper a [] = parseJSON a
+    helper (Object a) (x:xs) = do
+      field <- a .: x
+      helper field xs
+    helper a (x:xs) = fail $ "expect Object, but got " <> (show a)
+
+
+
+
+$(genParser "stargate" "characters")
+
+$(genProvider "stargate" "characters")
+
+
+$(genParser "stargate" "planets")
+
+$(genProvider "stargate" "planets")
+
+
+$(genParser "stargate" "quotes")
+
+$(genProvider "stargate" "quotes")
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Faker/Stargate.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Stargate where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.Stargate
+import Faker.TH
+
+
+$(generateFakeField "stargate" "characters")
+
+$(generateFakeField "stargate" "planets")
+
+$(generateFakeField "stargate" "quotes")
+
+
+
+
+
+
