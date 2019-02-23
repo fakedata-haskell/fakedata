0a38aee6ac2bd2f7216d4d051baea9ee743521e7
Sibi Prabakaran <sibi@psibi.in>
2019-02-23 14:52:14 +0530

    House module

---
+++ src/Faker/House.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.House where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.House
+import Faker.TH
+
+
+$(generateFakeField "house" "furniture")
+
+$(generateFakeField "house" "rooms")
+
+
+
+
+
+
