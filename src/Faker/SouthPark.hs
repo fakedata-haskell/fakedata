83b09465e6acc4170e22c7938879372528666103
Sibi Prabakaran <sibi@psibi.in>
2019-02-23 22:15:35 +0530

    SouthPark

---
+++ src/Faker/SouthPark.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.SouthPark where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.SouthPark
+import Faker.TH
+
+
+$(generateFakeField "southPark" "characters")
+
+$(generateFakeField "southPark" "quotes")
+
+
+
+
+
+
