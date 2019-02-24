3487e72cf3e7f12e011aae995abc97542df66553
Sibi Prabakaran <sibi@psibi.in>
2019-02-24 10:31:12 +0530

    TheItCrowd

---
+++ src/Faker/Provider/TheItCrowd.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Provider.TheItCrowd where
+
+import Config
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Data.Yaml
+import Faker
+import Faker.Internal
+import Faker.Provider.TH
+import Language.Haskell.TH
+
+parseTheItCrowd :: FromJSON a => FakerSettings -> Value -> Parser a
+parseTheItCrowd settings (Object obj) = do
+  en <- obj .: (getLocale settings)
+  faker <- en .: "faker"
+  theItCrowd <- faker .: "the_it_crowd"
+  pure theItCrowd
+parseTheItCrowd settings val = fail $ "expected Object, but got " <> (show val)
+
+parseTheItCrowdField ::
+     (FromJSON a, Monoid a) => FakerSettings -> Text -> Value -> Parser a
+parseTheItCrowdField settings txt val = do
+  theItCrowd <- parseTheItCrowd settings val
+  field <- theItCrowd .:? txt .!= mempty
+  pure field
+
+parseTheItCrowdFields ::
+     (FromJSON a, Monoid a) => FakerSettings -> [Text] -> Value -> Parser a
+parseTheItCrowdFields settings txts val = do
+  theItCrowd <- parseTheItCrowd settings val
+  helper theItCrowd txts
+  where
+    helper :: (FromJSON a) => Value -> [Text] -> Parser a
+    helper a [] = parseJSON a
+    helper (Object a) (x:xs) = do
+      field <- a .: x
+      helper field xs
+    helper a (x:xs) = fail $ "expect Object, but got " <> (show a)
+
+
+
+
+$(genParser "theItCrowd" "actors")
+
+$(genProvider "theItCrowd" "actors")
+
+
+$(genParser "theItCrowd" "characters")
+
+$(genProvider "theItCrowd" "characters")
+
+
+$(genParser "theItCrowd" "emails")
+
+$(genProvider "theItCrowd" "emails")
+
+
+$(genParser "theItCrowd" "quotes")
+
+$(genProvider "theItCrowd" "quotes")
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Faker/TheItCrowd.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.TheItCrowd where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.TheItCrowd
+import Faker.TH
+
+
+$(generateFakeField "theItCrowd" "actors")
+
+$(generateFakeField "theItCrowd" "characters")
+
+$(generateFakeField "theItCrowd" "emails")
+
+$(generateFakeField "theItCrowd" "quotes")
+
+
+
+
+
+
