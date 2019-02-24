90614076b1679f0bbe96e8661acc6f47919ff2de
Sibi Prabakaran <sibi@psibi.in>
2019-02-24 10:06:24 +0530

    SwordArtOnline

---
+++ src/Faker/Provider/SwordArtOnline.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Provider.SwordArtOnline where
+
+import Config
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Data.Yaml
+import Faker
+import Faker.Internal
+import Faker.Provider.TH
+import Language.Haskell.TH
+
+parseSwordArtOnline :: FromJSON a => FakerSettings -> Value -> Parser a
+parseSwordArtOnline settings (Object obj) = do
+  en <- obj .: (getLocale settings)
+  faker <- en .: "faker"
+  swordArtOnline <- faker .: "sword_art_online"
+  pure swordArtOnline
+parseSwordArtOnline settings val = fail $ "expected Object, but got " <> (show val)
+
+parseSwordArtOnlineField ::
+     (FromJSON a, Monoid a) => FakerSettings -> Text -> Value -> Parser a
+parseSwordArtOnlineField settings txt val = do
+  swordArtOnline <- parseSwordArtOnline settings val
+  field <- swordArtOnline .:? txt .!= mempty
+  pure field
+
+parseSwordArtOnlineFields ::
+     (FromJSON a, Monoid a) => FakerSettings -> [Text] -> Value -> Parser a
+parseSwordArtOnlineFields settings txts val = do
+  swordArtOnline <- parseSwordArtOnline settings val
+  helper swordArtOnline txts
+  where
+    helper :: (FromJSON a) => Value -> [Text] -> Parser a
+    helper a [] = parseJSON a
+    helper (Object a) (x:xs) = do
+      field <- a .: x
+      helper field xs
+    helper a (x:xs) = fail $ "expect Object, but got " <> (show a)
+
+
+
+
+$(genParser "swordArtOnline" "real_name")
+
+$(genProvider "swordArtOnline" "real_name")
+
+
+$(genParser "swordArtOnline" "game_name")
+
+$(genProvider "swordArtOnline" "game_name")
+
+
+$(genParser "swordArtOnline" "location")
+
+$(genProvider "swordArtOnline" "location")
+
+
+$(genParser "swordArtOnline" "item")
+
+$(genProvider "swordArtOnline" "item")
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Faker/SwordArtOnline.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.SwordArtOnline where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.SwordArtOnline
+import Faker.TH
+
+
+$(generateFakeField "swordArtOnline" "real_name")
+
+$(generateFakeField "swordArtOnline" "game_name")
+
+$(generateFakeField "swordArtOnline" "location")
+
+$(generateFakeField "swordArtOnline" "item")
+
+
+
+
+
+
