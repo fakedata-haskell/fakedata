50dedd6a1b4207047f38cb685baf8d73df3749e2
Sibi Prabakaran <sibi@psibi.in>
2019-02-24 10:32:53 +0530

    TheThickOfIt

---
+++ src/Faker/Provider/TheThickOfIt.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Provider.TheThickOfIt where
+
+import Config
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Data.Yaml
+import Faker
+import Faker.Internal
+import Faker.Provider.TH
+import Language.Haskell.TH
+
+parseTheThickOfIt :: FromJSON a => FakerSettings -> Value -> Parser a
+parseTheThickOfIt settings (Object obj) = do
+  en <- obj .: (getLocale settings)
+  faker <- en .: "faker"
+  theThickOfIt <- faker .: "the_thick_of_it"
+  pure theThickOfIt
+parseTheThickOfIt settings val = fail $ "expected Object, but got " <> (show val)
+
+parseTheThickOfItField ::
+     (FromJSON a, Monoid a) => FakerSettings -> Text -> Value -> Parser a
+parseTheThickOfItField settings txt val = do
+  theThickOfIt <- parseTheThickOfIt settings val
+  field <- theThickOfIt .:? txt .!= mempty
+  pure field
+
+parseTheThickOfItFields ::
+     (FromJSON a, Monoid a) => FakerSettings -> [Text] -> Value -> Parser a
+parseTheThickOfItFields settings txts val = do
+  theThickOfIt <- parseTheThickOfIt settings val
+  helper theThickOfIt txts
+  where
+    helper :: (FromJSON a) => Value -> [Text] -> Parser a
+    helper a [] = parseJSON a
+    helper (Object a) (x:xs) = do
+      field <- a .: x
+      helper field xs
+    helper a (x:xs) = fail $ "expect Object, but got " <> (show a)
+
+
+
+
+$(genParser "theThickOfIt" "characters")
+
+$(genProvider "theThickOfIt" "characters")
+
+
+$(genParser "theThickOfIt" "positions")
+
+$(genProvider "theThickOfIt" "positions")
+
+
+$(genParser "theThickOfIt" "departments")
+
+$(genProvider "theThickOfIt" "departments")
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Faker/TheThickOfIt.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.TheThickOfIt where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.TheThickOfIt
+import Faker.TH
+
+
+$(generateFakeField "theThickOfIt" "characters")
+
+$(generateFakeField "theThickOfIt" "positions")
+
+$(generateFakeField "theThickOfIt" "departments")
+
+
+
+
+
+
