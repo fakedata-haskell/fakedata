5d9b153a880c6198058cf0d9df78144d90a74de1
Sibi Prabakaran <sibi@psibi.in>
2019-02-24 10:34:07 +0530

    TwinPeaks

---
+++ src/Faker/Provider/TwinPeaks.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Provider.TwinPeaks where
+
+import Config
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import Data.Yaml
+import Faker
+import Faker.Internal
+import Faker.Provider.TH
+import Language.Haskell.TH
+
+parseTwinPeaks :: FromJSON a => FakerSettings -> Value -> Parser a
+parseTwinPeaks settings (Object obj) = do
+  en <- obj .: (getLocale settings)
+  faker <- en .: "faker"
+  twinPeaks <- faker .: "twin_peaks"
+  pure twinPeaks
+parseTwinPeaks settings val = fail $ "expected Object, but got " <> (show val)
+
+parseTwinPeaksField ::
+     (FromJSON a, Monoid a) => FakerSettings -> Text -> Value -> Parser a
+parseTwinPeaksField settings txt val = do
+  twinPeaks <- parseTwinPeaks settings val
+  field <- twinPeaks .:? txt .!= mempty
+  pure field
+
+parseTwinPeaksFields ::
+     (FromJSON a, Monoid a) => FakerSettings -> [Text] -> Value -> Parser a
+parseTwinPeaksFields settings txts val = do
+  twinPeaks <- parseTwinPeaks settings val
+  helper twinPeaks txts
+  where
+    helper :: (FromJSON a) => Value -> [Text] -> Parser a
+    helper a [] = parseJSON a
+    helper (Object a) (x:xs) = do
+      field <- a .: x
+      helper field xs
+    helper a (x:xs) = fail $ "expect Object, but got " <> (show a)
+
+
+
+
+$(genParser "twinPeaks" "characters")
+
+$(genProvider "twinPeaks" "characters")
+
+
+$(genParser "twinPeaks" "locations")
+
+$(genProvider "twinPeaks" "locations")
+
+
+$(genParser "twinPeaks" "quotes")
+
+$(genProvider "twinPeaks" "quotes")
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Faker/TwinPeaks.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.TwinPeaks where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.TwinPeaks
+import Faker.TH
+
+
+$(generateFakeField "twinPeaks" "characters")
+
+$(generateFakeField "twinPeaks" "locations")
+
+$(generateFakeField "twinPeaks" "quotes")
+
+
+
+
+
+
