e524eb38f7e6722b0b6e230ee38deb23878c974d
Sibi Prabakaran <sibi@psibi.in>
2019-03-01 23:27:51 +0530

    Missing modules

---
+++ src/Faker/Movie.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Movie where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.Movie
+import Faker.TH
+
+$(generateFakeField "movie" "quote")
---
+++ src/Faker/Quote.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Faker.Quote where
+
+import Data.Text
+import Faker
+import Faker.Internal
+import Faker.Provider.Quote
+import Faker.TH
+
+$(generateFakeField "quote" "famous_last_words")
+
+$(generateFakeField "quote" "matz")
+
+$(generateFakeField "quote" "most_interesting_man_in_the_world")
+
+$(generateFakeField "quote" "robin")
+
+$(generateFakeField "quote" "singular_siegler")
+
+$(generateFakeField "quote" "yoda")
