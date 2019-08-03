aa619ecffce646310aded7d95417498a9cd0cc28
Sibi Prabakaran <sibi@psibi.in>
2019-08-03 23:48:35 +0530

    ADd further implemenation

--- src/Faker/TH.hs
+++ src/Faker/TH.hs
@@ -7,7 +7,7 @@ module Faker.TH
   , generateFakeFields
   , generateFakeFieldUnresolved
   , generateFakeFieldUnresolveds
-  -- , generateFakeField2
+  , generateFakeField2
   ) where
 
 import Config
@@ -36,29 +36,31 @@ lowerTitle (x:xs) = (toLower x) : xs
 -- $(genrateFakeField "address" "state")
 -- The above splice will generate a function named state
 -- Assumes the presence of the function named "addressStateProvider"
--- generateFakeField :: String -> String -> Q [Dec]
--- generateFakeField entityName fieldName = do
---   let funName =
---         mkName $
---         case refinedString fieldName of
---           "type" -> "type'"
---           other -> other
---       pfn = refinedString $ entityName <> (stringTitle fieldName) <> "Provider"
---   providerName <- lookupValueName pfn
---   providerFn <-
---     case providerName of
---       Nothing ->
---         fail $ "generateFakefield: Function " <> pfn <> " not found in scope"
---       Just fn -> return fn
---   return $
---     [ SigD funName (AppT (ConT ''Fake) (ConT ''Text))
---     , ValD
---         (VarP funName)
---         (NormalB (AppE (ConE 'Fake) (AppE (VarE 'resolver) (VarE providerFn))))
---         []
---     ]
 generateFakeField :: String -> String -> Q [Dec]
 generateFakeField entityName fieldName = do
+  let funName =
+        mkName $
+        case refinedString fieldName of
+          "type" -> "type'"
+          other -> other
+      pfn = refinedString $ entityName <> (stringTitle fieldName) <> "Provider"
+  providerName <- lookupValueName pfn
+  providerFn <-
+    case providerName of
+      Nothing ->
+        fail $ "generateFakefield: Function " <> pfn <> " not found in scope"
+      Just fn -> return fn
+  return $
+    [ SigD funName (AppT (ConT ''Fake) (ConT ''Text))
+    , ValD
+        (VarP funName)
+        (NormalB (AppE (ConE 'Fake) (AppE (VarE 'resolver) (VarE providerFn))))
+        []
+    ]
+
+-- todo: replace this with above one
+generateFakeField2 :: String -> String -> Q [Dec]
+generateFakeField2 entityName fieldName = do
   let funName =
         mkName $
         case refinedString fieldName of
@@ -82,6 +84,34 @@ generateFakeField entityName fieldName = do
                  (AppE
                     (AppE (VarE 'cachedRandomVec) (LitE (StringL entityName)))
                     (LitE (StringL fieldName)))
+                 (VarE providerFn))))
+        []
+    ]
+
+generateFakeFields2 :: String -> [String] -> Q [Dec]
+generateFakeFields2 entityName fieldName = do
+  let fieldName' = map stringTitle fieldName
+      fieldName'' = concat fieldName'
+      pfn = refinedString $ entityName <> fieldName'' <> "Provider"
+      funNameString = lowerTitle $ refinedString fieldName''
+      funName = mkName funNameString
+  providerName <- lookupValueName pfn
+  providerFn <-
+    case providerName of
+      Nothing ->
+        fail $ "generateFakefield: Function " <> pfn <> " not found in scope"
+      Just fn -> return fn
+  return $
+    [ SigD funName (AppT (ConT ''Fake) (ConT ''Text))
+    , ValD
+        (VarP funName)
+        (NormalB
+           (AppE
+              (ConE 'Fake)
+              (AppE
+                 (AppE
+                    (AppE (VarE 'cachedRandomVec) (LitE (StringL entityName)))
+                    (LitE (StringL funNameString)))
                  (VarE providerFn))))
         []
     ]
