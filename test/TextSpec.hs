8e0c7fcdc5cd145e5289dfcde13423e9bf4218c3
Sibi Prabakaran <sibi@psibi.in>
2019-08-03 21:57:02 +0530

    Add test for address

---
+++ test/TextSpec.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module TextSpec where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Faker hiding (defaultFakerSettings)
+import qualified Faker.Address as FA
+import Test.Hspec
+import TestImport
+
+isText :: Text -> Bool
+isText x = T.length x >= 1
+
+verifyFakes :: [Fake Text] -> IO [Bool]
+verifyFakes funs = do
+  let fs :: [IO Text] = map generate funs
+      gs :: [IO Bool] = map (\f -> isText <$> f) fs
+  sequence gs
+
+spec :: Spec
+spec = do
+  describe "TextSpec" $ do
+    it "Address" $ do
+      let functions :: [Fake Text] =
+            [ FA.country
+            , FA.cityPrefix
+            , FA.citySuffix
+            , FA.countryCode
+            , FA.countryCodeLong
+            , FA.buildingNumber
+            , FA.communityPrefix
+            , FA.communitySuffix
+            , FA.community
+            , FA.streetSuffix
+            , FA.secondaryAddress
+            , FA.postcode
+            , FA.state
+            , FA.stateAbbr
+            , FA.timeZone
+            , FA.city
+            , FA.streetName
+            , FA.streetAddress
+            , FA.fullAddress
+            ]
+      bools <- verifyFakes functions
+      (and bools) `shouldBe` True
