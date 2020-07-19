e37a861ca06435c2ec37564f633bef3ef4045245
Sibi Prabakaran <sibi@psibi.in>
2020-07-19 12:00:42 +0530

    Add test for the Address module

--- test/AddressSpec.hs
+++ test/AddressSpec.hs
@@ -96,3 +96,68 @@ spec = do
         let action =
               generateWithSettings (setLocale "ee" defaultFakerSettings) country
         action `shouldThrow` fakerException
+    describe "Functions in address module" $ do
+      it "Country" $ do
+        val <- generateWithSettings defaultFakerSettings country
+        val `shouldBe` "Ecuador"
+      it "cityPrefix" $ do
+        val <- generateWithSettings defaultFakerSettings cityPrefix
+        val `shouldBe` "East"
+      it "citySuffix" $ do
+        val <- generateWithSettings defaultFakerSettings citySuffix
+        val `shouldBe` "burgh"
+      it "countryCode" $ do
+        val <- generateWithSettings defaultFakerSettings countryCode
+        val `shouldBe` "FJ"
+      it "countryCodeLong" $ do
+        val <- generateWithSettings defaultFakerSettings countryCodeLong
+        val `shouldBe` "CYM"
+      it "buildingNumber" $ do
+        val <- generateWithSettings defaultFakerSettings buildingNumber
+        val `shouldBe` "351"
+      it "communityPrefix" $ do
+        val <- generateWithSettings defaultFakerSettings communityPrefix
+        val `shouldBe` "Pine"
+      it "communitySuffix" $ do
+        val <- generateWithSettings defaultFakerSettings communitySuffix
+        val `shouldBe` "Oaks"
+      it "community" $ do
+        val <- generateWithSettings defaultFakerSettings community
+        val `shouldBe` "Pine Place"
+      it "streetSuffix" $ do
+        val <- generateWithSettings defaultFakerSettings streetSuffix
+        val `shouldBe` "Way"
+      it "secondaryAddress" $ do
+        val <- generateWithSettings defaultFakerSettings secondaryAddress
+        val `shouldBe` "Suite 351"
+      it "postcode" $ do
+        val <- generateWithSettings defaultFakerSettings postcode
+        val `shouldBe` "24351-4351"
+      it "state" $ do
+        val <- generateWithSettings defaultFakerSettings state
+        val `shouldBe` "Michigan"
+      it "stateAbbr" $ do
+        val <- generateWithSettings defaultFakerSettings stateAbbr
+        val `shouldBe` "MI"
+      it "timeZone" $ do
+        val <- generateWithSettings defaultFakerSettings timeZone
+        val `shouldBe` "America/Chicago"
+      it "city" $ do
+        val <- generateWithSettings defaultFakerSettings city
+        val `shouldBe` "East Vernita"
+      it "streetName" $ do
+        val <- generateWithSettings defaultFakerSettings streetName
+        val `shouldBe` "Langosh Ferry"
+      it "streetAddress" $ do
+        val <- generateWithSettings defaultFakerSettings streetAddress
+        val `shouldBe` "351 Vernita Avenue"
+      it "fullAddress" $ do
+        val <- generateWithSettings defaultFakerSettings fullAddress
+        val `shouldBe` "Suite 351 892 Steuber Points, Kelleymouth, AK 66043-6043"
+      it "mailBox" $ do
+        val <- generateWithSettings defaultFakerSettings mailBox
+        val `shouldBe` "PO Box 4351"
+      it "cityWithState" $ do
+        val <- generateWithSettings defaultFakerSettings cityWithState
+        val `shouldBe` "East Vernita, Minnesota"
+
